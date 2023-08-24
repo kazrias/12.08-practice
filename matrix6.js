baaa122394f76abd7d95bdd247ff61e854b6aac9
Artyom Harutyunyan <ka6rias@gmail.com>
2023-08-24 18:01:34 +0400

    correct version of 6th task

--- matrix6.js
+++ matrix6.js
@@ -1,1 +1,27 @@
-//
+function biggestLengthOfSquare(matrix = []) {
+  let maxSize = 0;
+  const len = matrix.length;
+  let board = Array.from({ length: len }, () => Array.from({ length: len }).fill(0))
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (matrix[i][j] === 1) {
+        if (i === 0 || j === 0) {
+          board[i][j] = 1;
+        } else {
+          board[i][j] =
+            Math.min(
+              board[i - 1][j],
+              board[i][j - 1],
+              board[i - 1][j - 1]
+            ) + 1;
+        }
+
+        if (board[i][j] > maxSize) {
+          maxSize = board[i][j];
+        }
+      }
+    }
+  }
+
+  return maxSize;
+}

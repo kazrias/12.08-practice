f4d3a0f9ced1cbae641f66044e819fd9abf12741
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-14 19:17:15 +0400

    matrix5 task fix

--- matrix5.js
+++ matrix5.js
@@ -1,63 +1,24 @@
-function largestNestedMatrix(matrix, k) {
-    let rows = matrix.length;
-    let cols = matrix.length;
-    let max_size = 0;
+function rowToColumn(matrix) {
     let result = [];
-
-    for (let row_start = 0; row_start < rows; row_start++) {
-        for (let col_start = 0; col_start < cols; col_start++) {
-            for (let row_end = row_start; row_end < rows; row_end++) {
-                for (let col_end = col_start; col_end < cols; col_end++) {
-                    let valid = true;
-                    for (let i = row_start; i <= row_end; i++) {
-                        let row_sum = 0;
-                        for (let j = col_start; j <= col_end; j++) {
-                            row_sum += matrix[i][j];
-                        }
-                        if (row_sum > k) {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    for (let j = col_start; j <= col_end; j++) {
-                        let col_sum = 0;
-                        for (let i = row_start; i <= row_end; i++) {
-                            col_sum += matrix[i][j];
-                        }
-                        if (col_sum > k) {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    if (valid) {
-                        let size = (row_end - row_start + 1) * (col_end - col_start + 1);
-                        if (size > max_size) {
-                            max_size = size;
-                            result = [];
-                            for (let i = row_start; i <= row_end; i++) {
-                                let row = [];
-                                for (let j = col_start; j <= col_end; j++) {
-                                    row.push(matrix[i][j]);
-                                }
-                                result.push(row);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    for (let i = 0; i < matrix[0].length; i++) {
+        let row = matrix.map(e => e[i]);
+        result.push(row);
     }
-
     return result;
 }
 
-let matrix = [
-    [1, 1, 1, 1, 1, 1],
-    [4, 0, 4, 0, 4, 0],
-    [3, 3, 3, 3, 3, 3],
-    [0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1],
-    [15, 15, 15, 15, 15, 15]]
-let k = 19;
-let result = largestNestedMatrix(matrix, k);
-console.log(result);
+function checkSum(matrix, k) {
+    let res = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i].reduce((curr, next) => curr + next, 0) < k)
+            res.push(matrix[i])
+    }
+    return res
+}
+
+function largestNestedMatrix(matrix, k) {
+    let rowsChecked = checkSum(matrix, k)
+    let rotatedMatrix = rowToColumn(rowsChecked);
+    let columnsChecked = checkSum(rotatedMatrix, k)
+    return rowToColumn(columnsChecked)
+}
